b6d4bcef434e2947c82cac452d328d1407bef358
Peter <petersolomon704@gmail.com>
2023-04-15 20:04:45 +0100

    delete blog
    Added delete blog

--- controllers/blog.js
+++ controllers/blog.js
@@ -3,7 +3,6 @@ const User = require("../models/User");
 
 exports.addPost = async (req, res) => {
   const body = req.body;
-  console.log("BODY", body);
   try {
     const user = await User.findById(req.body.userId);
     const newBlog = new Blog({
@@ -33,5 +32,24 @@ exports.getBlogs = async (req, res) => {
     res.status(200).json({ result: posts });
   } catch (error) {
     res.status(403).json(error);
+  }
+};
+
+exports.deleteBlog = async (req, res) => {
+  try {
+    const id = req.params.id;
+    const user = req.user;
+    const blog = await Blog.findById(id);
+    if (blog.userId.toString() !== user.toString()) {
+      return res.status(403).json("You cannot delete this blog");
+    }
+    const deletedBlog = await Blog.findByIdAndRemove(id);
+    if (!deletedBlog) {
+      res.status(400).json("Could not delete blog");
+    } else {
+      res.status(200).json("Blog deleted");
+    }
+  } catch (error) {
+    res.status(500).json(error);
   }
 };
